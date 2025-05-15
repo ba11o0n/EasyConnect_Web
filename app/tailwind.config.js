450ebbc3d206aba4ab18c579970f8d51a34e0cb5
Kathleen Ngo <khngo@ucsd.edu>
2025-05-15 09:03:08 -0700

    Working website with all frontend except for employee

---
+++ app/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: [
+        "./index.html",
+        "./setup-profile.html",
+        "./dashboard.html",
+        "./src/**/*.{js,html}",
+        "./*.html", "./src/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          display: ['"Poppins"', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+};
+  
